6ee0b4f527c771b357780ff0ef9aaddf74ae8327
nickxiaohk01 <nickxiao@hk01.com>
2021-02-16 18:45:20 +0800

    feat: draft out the checkout page

--- src/Checkout/checkout.jsx
+++ src/Checkout/checkout.jsx
@@ -1,182 +1,186 @@
-import React, { Fragment } from 'react';
-import { formatMoney } from 'accounting';
-import { createCheckoutService } from '@bigcommerce/checkout-sdk';
-import Panel from '../components/Panel/panel';
-import SubmitButton from '../components/SubmitButton/submit-button';
-import Billing from '../Billing/billing';
-import Cart from '../Cart/cart';
-import Customer from '../Customer/customer';
-import LoginPanel from '../LoginPanel/login-panel';
-import Payment from '../Payment/payment';
-import Shipping from '../Shipping/shipping';
-import Layout from './Layout/layout';
-import LoadingState from './LoadingState/loading-state';
-import styles from './checkout.scss';
+import React, { Fragment } from 'react'
+import { formatMoney } from 'accounting'
+import { createCheckoutService } from '@bigcommerce/checkout-sdk'
+import Panel from '../components/Panel/panel'
+import SubmitButton from '../components/SubmitButton/submit-button'
+import Billing from '../Billing/billing'
+import Cart from '../Cart/cart'
+import Customer from '../Customer/customer'
+import LoginPanel from '../LoginPanel/login-panel'
+import Payment from '../Payment/payment'
+import Shipping from '../Shipping/shipping'
+import Layout from './Layout/layout'
+import LoadingState from './LoadingState/loading-state'
+import styles from './checkout.scss'
 
 export default class Checkout extends React.PureComponent {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props)
 
-        this.service = createCheckoutService();
+    this.service = createCheckoutService()
 
-        this.state = {
-            isPlacingOrder: false,
-            showSignInPanel: false,
-        };
+    this.state = {
+      isPlacingOrder: false,
+      showSignInPanel: false,
+    }
+  }
+
+  componentDidMount() {
+    Promise.all([
+      this.service.loadCheckout(),
+      this.service.loadShippingCountries(),
+      this.service.loadShippingOptions(),
+      this.service.loadBillingCountries(),
+      this.service.loadPaymentMethods(),
+    ]).then(() => {
+      this.unsubscribe = this.service.subscribe((state) => {
+        this.setState(state)
+      })
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe()
+  }
+
+  render() {
+    const { data, errors, statuses } = this.state
+
+    if (!data) {
+      return <Layout body={<LoadingState />} />
     }
 
-    componentDidMount() {
-        Promise.all([
-            this.service.loadCheckout(),
-            this.service.loadShippingCountries(),
-            this.service.loadShippingOptions(),
-            this.service.loadBillingCountries(),
-            this.service.loadPaymentMethods(),
-        ]).then(() => {
-            this.unsubscribe = this.service.subscribe((state) => {
-                this.setState(state);
-            });
-        });
+    if (this.state.showSignInPanel) {
+      return (
+        <Layout
+          body={
+            <LoginPanel
+              errors={errors.getSignInError()}
+              isSigningIn={statuses.isSigningIn()}
+              onClick={(customer) =>
+                this.service
+                  .signInCustomer(customer)
+                  .then(() => this.service.loadShippingOptions())
+              }
+              onClose={() => this.setState({ showSignInPanel: false })}
+            />
+          }
+        />
+      )
     }
 
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
+    return (
+      <Layout
+        body={
+          <Fragment>
+            <div className={styles.body}>
+              <Panel
+                body={
+                  <form
+                    onSubmit={(event) =>
+                      this._submitOrder(event, data.getCustomer().isGuest)
+                    }
+                  >
+                    <Shipping
+                      customer={data.getCustomer()}
+                      consignments={data.getConsignments()}
+                      cart={data.getCart()}
+                      isUpdatingConsignment={statuses.isUpdatingConsignment}
+                      isCreatingConsignments={statuses.isCreatingConsignments}
+                      isUpdatingShippingAddress={
+                        statuses.isUpdatingShippingAddress
+                      }
+                      address={data.getShippingAddress()}
+                      countries={data.getShippingCountries()}
+                      options={data.getShippingOptions()}
+                      selectedOptionId={
+                        data.getSelectedShippingOption()
+                          ? data.getSelectedShippingOption().id
+                          : ''
+                      }
+                      isSelectingShippingOption={
+                        statuses.isSelectingShippingOption
+                      }
+                      onShippingOptionChange={(optionId) =>
+                        this.service.selectShippingOption(optionId)
+                      }
+                      onConsignmentUpdate={(consignment) =>
+                        consignment.id
+                          ? this.service.updateConsignment(consignment)
+                          : this.service.createConsignments([consignment])
+                      }
+                      onAddressChange={(shippingAddress) => {
+                        this.setState({ shippingAddress })
+                        this.service.updateShippingAddress(shippingAddress)
+                      }}
+                    />
 
-    render() {
-        const { data, errors, statuses } = this.state;
-
-        if (!data) {
-            return (
-                <Layout body={
-                    <LoadingState />
-                } />
-            );
-        }
-
-        if (this.state.showSignInPanel) {
-            return (
-                <Layout body={
-                    <LoginPanel
-                        errors={ errors.getSignInError() }
-                        isSigningIn={ statuses.isSigningIn() }
-                        onClick={ (customer) => this.service.signInCustomer(customer)
-                            .then(() => this.service.loadShippingOptions())
+                    <div className={styles.actionContainer}>
+                      <SubmitButton
+                        label={
+                          this._isPlacingOrder()
+                            ? 'Placing your order...'
+                            : `Pay ${formatMoney(
+                                data.getCheckout().grandTotal
+                              )}`
                         }
-                        onClose={ () => this.setState({ showSignInPanel: false }) } />
-                } />
-            );
+                        isLoading={this._isPlacingOrder()}
+                      />
+                    </div>
+                  </form>
+                }
+              />
+            </div>
+
+            <div className={styles.side}>
+              <Cart
+                checkout={data.getCheckout()}
+                cartLink={data.getConfig().links.cartLink}
+              />
+            </div>
+          </Fragment>
         }
+      />
+    )
+  }
 
-        return (
-            <Layout body={
-                <Fragment>
-                    <div className={ styles.body }>
-                        <Panel body={
-                            <form onSubmit={ (event) => this._submitOrder(event, data.getCustomer().isGuest) }>
-                                <Customer
-                                    customer={ data.getCustomer() }
-                                    billingAddress={ data.getBillingAddress() }
-                                    isSigningOut={ statuses.isSigningOut() }
-                                    onClick={ () => this.service.signOutCustomer()
-                                        .then(() => this.service.loadShippingOptions()) }
-                                    onChange={ (customer) => this.setState({ customer }) }
-                                    onSignIn={ () => this.setState({ showSignInPanel: true }) } />
+  _isPlacingOrder() {
+    const { statuses } = this.state
 
-                                <Shipping
-                                    customer={ data.getCustomer() }
-                                    consignments={ data.getConsignments() }
-                                    cart={ data.getCart() }
-                                    isUpdatingConsignment={ statuses.isUpdatingConsignment }
-                                    isCreatingConsignments={ statuses.isCreatingConsignments }
-                                    isUpdatingShippingAddress={ statuses.isUpdatingShippingAddress }
-                                    address={ data.getShippingAddress() }
-                                    countries={ data.getShippingCountries() }
-                                    options={ data.getShippingOptions() }
-                                    selectedOptionId={ data.getSelectedShippingOption() ? data.getSelectedShippingOption().id : '' }
-                                    isSelectingShippingOption ={ statuses.isSelectingShippingOption }
-                                    onShippingOptionChange={ (optionId) => this.service.selectShippingOption(optionId) }
-                                    onConsignmentUpdate={ (consignment) => (
-                                        consignment.id ?
-                                            this.service.updateConsignment(consignment) :
-                                            this.service.createConsignments([consignment])
-                                        )
-                                    }
-                                    onAddressChange={ (shippingAddress) => {
-                                        this.setState({ shippingAddress })
-                                        this.service.updateShippingAddress(shippingAddress)
-                                    }} />
+    return (
+      this.state.isPlacingOrder &&
+      (statuses.isSigningIn() ||
+        statuses.isUpdatingShippingAddress() ||
+        statuses.isUpdatingBillingAddress() ||
+        statuses.isSubmittingOrder())
+    )
+  }
 
-                                <Payment
-                                    errors={ errors.getSubmitOrderError() }
-                                    methods={ data.getPaymentMethods() }
-                                    onClick={ (name, gateway) => this.service.initializePayment({ methodId: name, gatewayId: gateway }) }
-                                    onChange={ (payment) => this.setState({ payment }) } />
+  _submitOrder(event, isGuest) {
+    let billingAddressPayload = this.state.billingAddressSameAsShippingAddress
+      ? this.state.shippingAddress
+      : this.state.billingAddress
 
-                                <Billing
-                                    multishipping={ (data.getConsignments() || []).length > 1 }
-                                    address={ data.getBillingAddress() }
-                                    countries={ data.getBillingCountries() }
-                                    sameAsShippingAddress={
-                                        (this.state.billingAddressSameAsShippingAddress === undefined) ||
-                                        this.state.billingAddressSameAsShippingAddress
-                                    }
-                                    onChange ={ (billingAddress) => this.setState({ billingAddress }) }
-                                    onSelect ={ (billingAddressSameAsShippingAddress) => this.setState({ billingAddressSameAsShippingAddress })  } />
-
-                                <div className={ styles.actionContainer }>
-                                    <SubmitButton
-                                        label={ this._isPlacingOrder() ?
-                                            'Placing your order...' :
-                                            `Pay ${ formatMoney((data.getCheckout()).grandTotal) }`
-                                        }
-                                        isLoading={ this._isPlacingOrder() } />
-                                </div>
-                            </form>
-                        } />
-                    </div>
-
-                    <div className={ styles.side }>
-                        <Cart
-                            checkout={ data.getCheckout() }
-                            cartLink={ (data.getConfig()).links.cartLink } />
-                    </div>
-                </Fragment>
-            } />
-        );
+    billingAddressPayload = {
+      ...billingAddressPayload,
+      email: this.state.customer.email,
     }
 
-    _isPlacingOrder() {
-        const { statuses } = this.state;
+    let { payment } = this.state
 
-        return this.state.isPlacingOrder && (
-            statuses.isSigningIn() ||
-            statuses.isUpdatingShippingAddress() ||
-            statuses.isUpdatingBillingAddress() ||
-            statuses.isSubmittingOrder()
-        );
-    }
+    this.setState({ isPlacingOrder: true })
+    event.preventDefault()
 
-    _submitOrder(event, isGuest) {
-        let billingAddressPayload = this.state.billingAddressSameAsShippingAddress ?
-            this.state.shippingAddress :
-            this.state.billingAddress;
-
-        billingAddressPayload = { ...billingAddressPayload, email: this.state.customer.email };
-
-        let { payment } = this.state;
-
-        this.setState({ isPlacingOrder: true });
-        event.preventDefault();
-
-        Promise.all([
-            isGuest ? this.service.continueAsGuest(this.state.customer) : Promise.resolve(),
-            this.service.updateBillingAddress(billingAddressPayload),
-        ])
-            .then(() => this.service.submitOrder({ payment }))
-            .then(({ data }) => {
-                window.location.href = data.getConfig().links.orderConfirmationLink;
-            })
-            .catch(() => this.setState({ isPlacingOrder: false }));
-    }
+    Promise.all([
+      isGuest
+        ? this.service.continueAsGuest(this.state.customer)
+        : Promise.resolve(),
+      this.service.updateBillingAddress(billingAddressPayload),
+    ])
+      .then(() => this.service.submitOrder({ payment }))
+      .then(({ data }) => {
+        window.location.href = data.getConfig().links.orderConfirmationLink
+      })
+      .catch(() => this.setState({ isPlacingOrder: false }))
+  }
 }
--- src/components/Section/section.jsx
+++ src/components/Section/section.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
-import styles from './section.scss';
+import React from 'react'
+import styles from './section.scss'
 
 export default class Section extends React.PureComponent {
-    render() {
-        return (
-            <div className={ styles.section }>
-                <div className={ styles.header }>
-                    { this.props.header }
-                </div>
-
-                <div className={ styles.body }>
-                    { this.props.body }
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={styles.section}>
+        <div className={styles.header}>{this.props.header}</div>
+        <div className={styles.subHeader}>{this.props.subHeader}</div>
+        <div className={styles.body}>{this.props.body}</div>
+      </div>
+    )
+  }
 }
